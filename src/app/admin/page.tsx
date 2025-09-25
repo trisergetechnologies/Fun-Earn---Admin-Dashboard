fdb889708cf02bc1ed5b015759b0e63cc476b59f
Vaibhav Pandey <vaibhavkpandey7@gmail.com>
2025-09-25 16:29:45 +0530

    packages in dashboard

--- src/app/admin/page.tsx
+++ src/app/admin/page.tsx
@@ -10,6 +10,7 @@ import {
   Wallet as WalletIcon,
   ShoppingBasket as ShoppingBasketIcon,
   Settings as SettingsIcon,
+  Package,
 } from "lucide-react";
 
 const menu = [
@@ -19,6 +20,7 @@ const menu = [
   { icon: <TimeIcon className="w-6 h-6" />, name: "Watch Hours", path: "/admin/watch-hours" },
   { icon: <WalletIcon className="w-6 h-6" />, name: "System Wallet & Logs", path: "/admin/system-wallet" },
   { icon: <ShoppingBasketIcon className="w-6 h-6" />, name: "Orders", path: "/admin/orders" },
+  { icon: <Package className="w-6 h-6" />, name: "Packages", path: "/admin/package" },
   { icon: <SettingsIcon className="w-6 h-6" />, name: "Settings", path: "/admin/settings" },
 ];
 
@@ -69,52 +71,3 @@ export default function AdminWelcome() {
   );
 }
 
-
-
-
-
-
-
-
-// import type { Metadata } from "next";
-// import { EcommerceMetrics } from "@/components/ecommerce/EcommerceMetrics";
-// import React from "react";
-// import MonthlyTarget from "@/components/ecommerce/MonthlyTarget";
-// import MonthlySalesChart from "@/components/ecommerce/MonthlySalesChart";
-// import StatisticsChart from "@/components/ecommerce/StatisticsChart";
-// import RecentOrders from "@/components/ecommerce/RecentOrders";
-// import DemographicCard from "@/components/ecommerce/DemographicCard";
-
-// export const metadata: Metadata = {
-//   title:
-//     "Next.js E-commerce Dashboard | TailAdmin - Next.js Dashboard Template",
-//   description: "This is Next.js Home for TailAdmin Dashboard Template",
-// };
-
-// export default function Ecommerce() {
-//   return (
-//     <div className="grid grid-cols-12 gap-4 md:gap-6">
-//       <div className="col-span-12 space-y-6 xl:col-span-7">
-//         <EcommerceMetrics />
-
-//         <MonthlySalesChart />
-//       </div>
-
-//       <div className="col-span-12 xl:col-span-5">
-//         <MonthlyTarget />
-//       </div>
-
-//       <div className="col-span-12">
-//         <StatisticsChart />
-//       </div>
-
-//       <div className="col-span-12 xl:col-span-5">
-//         <DemographicCard />
-//       </div>
-
-//       <div className="col-span-12 xl:col-span-7">
-//         <RecentOrders />
-//       </div>
-//     </div>
-//   );
-// }
